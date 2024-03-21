05ada66909dcf3f1922f94f0594c79b053edaf2d
Shubham Chauhan <schauhan@deqode.com>
2024-03-21 23:53:53 +0530

    fix: build issue

--- src/components/organisms/navbar/NavBar.tsx
+++ src/components/organisms/navbar/NavBar.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useLocation, Link as RouterLink } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import Container from "@mui/material/Container";
 import Stack from "@mui/material/Stack";
 import Box from "@mui/material/Box";
